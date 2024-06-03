c810bc593511eedd9853abf219a0f42c71d3b93a
ThefaAPZ <mireya.apaza@tecsup.edu.pe>
2024-06-03 16:53:29 -0500

    Initial Commit

---
+++ UsodeControlesCuicapuza1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsodeControlesCuicapuza1
+//
+//  Created by Janco Flores on 3/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
